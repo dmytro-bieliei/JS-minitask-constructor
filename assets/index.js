48adfeddf2f68a2ff4dfe43a3168fcd4172d7550
dmytro-bieliei <dmbieliei@gmail.com>
2022-09-07 21:33:00 +0300

    add first task

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,13 @@
- 
+// 1-st task
+const age = +prompt('Enter your age');
+let isAdult = function( age ) {
+  if ( age > 17 ) {
+    isAdult = true;
+  } else {
+    isAdult = false;
+  }
+  return alert(isAdult);
+}
+isAdult(age);
+
+
