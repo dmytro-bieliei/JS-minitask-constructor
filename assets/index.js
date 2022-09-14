0fc6c842c7c971cb8c1c38bf33fef16c18ea62ee
dmytro-bieliei <dmbieliei@gmail.com>
2022-09-14 22:20:18 +0300

    add logIn logOut functions

--- assets/index.js
+++ assets/index.js
@@ -9,16 +9,28 @@ function User(firstName, lastName, email, age, isLoggedIn){
 };
 
 const userProto = {
-  getFullName: function( user ){
-    console.log(`${user} full name is ${this.firstName} ${this.lastName}`);
+  getFullName: function(){
+    return `Full name is ${this.firstName} ${this.lastName}`;
+  },
+  logIn: function(){
+    const loggetIn = this.isLoggedIn = true;
+    return loggetIn;
+  },
+  logOut: function(){
+    const loggetOut = this.isLoggedIn = false;
+    return loggetOut;
+  },
+  checkUserStatusLoggedIn: function(){
+    const result = (`${this.isLoggedIn}` === this.logIn) ? 'is logged in' : 'not logged in';
+      return result;
   },
 };
-  
+
 User.prototype = userProto;
 
-const user1 = new User('Dmytro', 'Bieliei', 'dmbieliei@gmail.com', '32', true);
-const user2 = new User('Ivan', 'Radkin', 'radkin@gmain.com', '46', false);
-const user3 = new User('Marina', 'Savitska', 'nolik@i.ua', '23', true);
+const user1 = new User('Dmytro', 'Bieliei', 'dmbieliei@gmail.com', 32,);
+const user2 = new User('Ivan', 'Radkin', 'radkin@gmain.com', 46,);
+const user3 = new User('Marina', 'Savitska', 'nolik@i.ua', 23,);
 
 
 
